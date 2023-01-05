6c2a54a15d37c11ee63f5d35abb7dc4c9753acfb
C3H3_Ttigone <2523849921@qq.com>
2023-01-06 00:13:34 +0800

    modified:   .vscode/exe/1.exe 	modified:   1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,40 +2,52 @@
 #include <stdlib.h>
 #include <string>
 using namespace std;
-class Person
+class Person;
+class collect
 {
 public:
+    collect();
+    void display1();
+    void display2();
+    Person * da;
+};
+class Person
+{
+    friend void collect::display2(); 
+public:
+    Person();
     int age;
-    Person(int age)
-    {
-        this->age = age;
-    }
-    Person& pap(Person & p)
-    {
-        this->age += p.age;
-        return * this;
-    }
+private:
+    string gender;
 };
 
-
+Person::Person()
+{
+    gender = "ÄÐ";
+    age = 19;
+}
+collect::collect()
+{
+    da = new Person;
+}
+void collect::display1()
+{
+    cout << da->age << endl;
+}
+void collect::display2()
+{
+    cout << da->age << endl;
+    cout << da->gender << endl;
+}
 void test()
 {
-    Person p(10);
-    Person p2(10);
-    p2.pap(p).pap(p);
-    cout << p.age << endl;
-    cout << p2.age << endl;
+    collect data;
+    data.display1();
+    data.display2();
 }
-
-
 int main()
 {
-    // test();
-    int age;
-    int * p = &age;
-     
-    cout << p << endl;
-    cout << &(*p) << endl;
+    test();
     system("pause");
     return 0;
 }
