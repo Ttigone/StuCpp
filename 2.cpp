16758b0fb628db394d8468b4b2307ddab58fdac0
Ttigone <2523849921@qq.com>
2023-01-07 11:02:13 +0800

    modified:   .vscode/exe/2.exe 	modified:   .vscode/settings.json 	modified:   2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,25 +1,54 @@
 #include <iostream>
-#include <string>
 #include <stdlib.h>
 using namespace std;
-class Person
+class dizen
 {
-    friend ostream& operator<<(ostream &cout, Person &p);
-private:
-    int m_A = 20;
-    int m_B = 10;
+    friend ostream& operator<<(ostream &out, dizen p);
+public:
+    dizen& operator++()
+    {
+        ++m_A;
+        return *this;
+    }    
+    dizen operator++(int)
+    {
+        dizen temp;
+        temp = *this;
+        m_A++;
+        cout << temp.m << endl;
+        delete temp.m;
+        cout << this->m << endl;
+        // delete this->m;
+        return temp;
+    }
+    dizen& operator=(dizen &p)
+    {
+        if(this->m != NULL){
+            delete m;
+            m = NULL;
+        }
+        m = new int(* p.m);
+        return *this;
+    }
+public:
+    int m_A = 1;
+    int * m = new int;
 };
-ostream& operator<<(ostream &cout, Person &p)
+ostream& operator<<(ostream &out, dizen p)
 {
-    cout << "m_A = "  << p.m_A << " m_B = " << p.m_B << endl;
-    return cout;
+    out << p.m_A << endl;
+    return out;
 }
 void test()
 {
-    Person p;
-    cout << p << endl;
+    dizen t;
+    // delete t.m;
+    cout << t++ << endl;
+    delete t.m;
+    cout << t << endl;
+    cout << t.m << endl;
+    // delete t.m;
 }
-
 
 int main()
 {
