9e146cf586439134c34070e01298ed7351d0b083
Ttigone <2523849921@qq.com>
2023-01-08 19:58:46 +0800

    modified:   .vscode/exe/2.exe 	new file:   .vscode/exe/3.exe 	new file:   .vscode/exe/c.exe 	deleted:    1.cpp 	deleted:    2.cpp 	new file:   c.c 	deleted:    exercise.cpp 	new file:   jicheng.cpp

---
+++ jicheng.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+#include <cstdio>
+
+
+
+int main()
+{
+    char choice[2][100];
+    char[0] = "选项0";
+    char[1] = "选项1";
+    system("pause");
+    return 0;
+}
