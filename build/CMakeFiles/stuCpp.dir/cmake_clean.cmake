0274663aafe047110c513436ae8325d152f85263
Ttigone <2523849921@qq.com>
2023-01-09 11:17:41 +0800

    new file:   jichen2.cpp 	modified:   jicheng.cpp

---
+++ build/CMakeFiles/stuCpp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stuCpp.dir/jichen2.obj"
+  "CMakeFiles/stuCpp.dir/jichen2.obj.d"
+  "CMakeFiles/stuCpp.dir/jicheng.obj"
+  "CMakeFiles/stuCpp.dir/jicheng.obj.d"
+  "libstuCpp.dll.a"
+  "stuCpp.exe"
+  "stuCpp.exe.manifest"
+  "stuCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stuCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
